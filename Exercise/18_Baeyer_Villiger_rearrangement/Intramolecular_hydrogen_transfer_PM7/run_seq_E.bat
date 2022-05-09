f72927391cd5b21f480c614e0b226d0ddd578201
by-student-2017 <studentsctest@gmail.com>
2022-05-09 23:24:54 +0900

    windows10

---
+++ Exercise/18_Baeyer_Villiger_rearrangement/Intramolecular_hydrogen_transfer_PM7/run_seq_E.bat
@@ -0,0 +1,20 @@
+set Series=E
+
+set JMol=C:\jmol-14.30.2\Jmol.jar
+
+
+"C:\Program Files\MOPAC\MOPAC2016.exe" %Series%1_SADDLE.mop
+
+pause
+
+"C:\Program Files\MOPAC\MOPAC2016.exe" %Series%2_TS.mop
+
+"C:\Program Files\MOPAC\MOPAC2016.exe" %Series%3_FORCE.mop
+
+"C:\Program Files\MOPAC\MOPAC2016.exe" %Series%4_IRC_products.mop
+
+start %JMol% %Series%4_IRC_products.xyz
+
+"C:\Program Files\MOPAC\MOPAC2016.exe" %Series%4_IRC_reactions.mop
+
+%JMol% %Series%4_IRC_reactions.xyz
