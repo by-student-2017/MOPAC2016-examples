d99e04a713eeccb275b83df435d071df5551eba1
by-student-2017 <studentsctest@gmail.com>
2022-05-14 10:23:33 +0900

    windows10

--- Exercise/02_IRC_method_map/PM6-D3H4X/run_NLLSQ_A.bat
+++ Exercise/02_IRC_method_map/PM6-D3H4X/run_NLLSQ_A.bat
@@ -3,8 +3,8 @@ set Series=A
 set MOPAC2016_EXE="C:\Program Files\MOPAC\MOPAC2016.exe"
 set MOPAC_EXE="C:\Program Files\MOPAC\bin\mopac.exe"
 
-if exist %MOPAC_EXE% ( set MOPAC_EXE = %MOPAC_EXE% ) 
-if exist %MOPAC2016_EXE% ( set MOPAC_EXE = %MOPAC_EXE% ) 
+if exist %MOPAC_EXE% ( set MOPAC_EXE=%MOPAC_EXE% ) 
+if exist %MOPAC2016_EXE% ( set MOPAC_EXE=%MOPAC_EXE% ) 
 
 
 %MOPAC_EXE% %Series%3_NLLSQ.mop
--- Exercise/08_Claisen_Schmidt_reaction/cis/PM3/run_TS_C.bat
+++ Exercise/08_Claisen_Schmidt_reaction/cis/PM3/run_TS_C.bat
@@ -3,7 +3,7 @@ set Series=C
 set MOPAC2016_EXE="C:\Program Files\MOPAC\MOPAC2016.exe"
 set MOPAC_EXE="C:\Program Files\MOPAC\bin\mopac.exe"
 
-if exist %MOPAC_EXE% ( set MOPAC_EXE = %MOPAC_EXE% ) 
-if exist %MOPAC2016_EXE% ( set MOPAC_EXE = %MOPAC_EXE% )
+if exist %MOPAC_EXE% ( set MOPAC_EXE=%MOPAC_EXE% ) 
+if exist %MOPAC2016_EXE% ( set MOPAC_EXE=%MOPAC_EXE% ) 
 
 %MOPAC_EXE% %Series%2_TS.mop
