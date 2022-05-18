0e8ea01fa535630527361ad328719c028a69f5d7
by-student-2017 <studentsctest@gmail.com>
2022-05-18 19:14:01 +0900

    windows10

---
+++ Solid_State/diamond_electron_band/run_2_mopac.bat
@@ -0,0 +1,4 @@
+
+REM "C:\Program Files\MOPAC\bin\mopac.exe" diamond.mop
+
+"C:\Program Files\MOPAC\mopac2016.exe" diamond.mop
---
+++ Solid_State/diamond_phonon_band/run_2_mopac.bat
@@ -0,0 +1,4 @@
+
+REM "C:\Program Files\MOPAC\bin\mopac.exe" diamond_phonon.mop
+
+"C:\Program Files\MOPAC\mopac2016.exe" diamond_phonon.mop
