9dc282087cff0a4262d49d89da0ddd0d487e7839
by-student-2017 <studentsctest@gmail.com>
2022-05-19 23:20:00 +0900

    windows10

---
+++ Solid_State/Graphite/graphite_phonon_band/run_seq.bat
@@ -0,0 +1,15 @@
+set INPUT_FILE=graphite
+
+set MOPAC2016_EXE="C:\Program Files\MOPAC\MOPAC2016.exe"
+set MOPAC_EXE="C:\Program Files\MOPAC\bin\mopac.exe"
+REM set MAKPOL_EXE="C:\Program Files\MOPAC\bin\mopac-mackpol.exe"
+  
+REM if exist %MOPAC_EXE% ( set MOPAC_EXE=%MOPAC_EXE% ) 
+if exist %MOPAC2016_EXE% (
+  set MOPAC_EXE=%MOPAC2016_EXE% 
+  REM set MAKPOL_EXE=".\..\MAKPOL_F90.exe"
+) 
+
+REM call %MAKPOL_EXE% Make_%INPUT_FILE%.dat
+call %MOPAC_EXE%  %INPUT_FILE%.mop
+call .\..\bz.exe %INPUT_FILE%.brz
