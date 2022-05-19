1825f8cf662dd4789e99d66f11b75c6b89011003
by-student-2017 <studentsctest@gmail.com>
2022-05-19 22:43:40 +0900

    windows10

---
+++ Solid_State/Diamond/diamond_electron_band_test/run_3_bz.bat
@@ -0,0 +1,4 @@
+
+REM "C:\Program Files\MOPAC\bin\mopac-bz.exe" diamond.brz
+
+.\..\bz.exe diamond.brz
---
+++ Solid_State/Diamond/diamond_phonon_band_test/run_3_bz.bat
@@ -0,0 +1,4 @@
+
+REM "C:\Program Files\MOPAC\bin\mopac-bz.exe" diamond_phonon.brz
+
+.\..\bz.exe diamond_phonon.brz
